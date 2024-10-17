ba1ba72e87587d1f1d24e12dc4081140e7da4ac7
Arun raj <riderarunraj@live.com>
2024-10-17 11:26:26 +0530

    Day 1
    Javascript

---
+++ Javascript/Day1/script.js
@@ -0,0 +1,134 @@
+// case-sensitive 
+// TotalAmount 
+// totalAmount  
+
+
+
+// onclick 
+// 
+
+
+
+// primitive types
+
+// number 
+// string 
+// boolian 
+// null
+// undefined 
+
+
+// number 
+
+// var a = 10;
+// var b = 10.0;
+
+// let c = 1e1; 
+
+
+// console.log(c);
+// console.log(a+c);
+
+// 1 , 209 , -8 , 4.6 , -99.99999 , 3e2.
+
+
+// NaN , Infinity,
+
+
+
+// string 
+
+// var s = "Hekko world" ;
+
+// var t = 6677;
+
+
+// console.log(s+'   \n          '+t);
+
+
+
+//boolian 
+// var b = true ;
+
+
+// console.log(b);
+
+
+
+// null , undefined , 
+
+
+
+
+// var book ; 
+
+
+// console.log(book);
+
+
+// console.log(typeof c);
+
+
+
+
+// var book = null ;
+
+// console.log(book);
+
+// var book = "ar";
+
+
+// console.log(book);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cost ; 
+
+
+// var cost , profit ; 
+
+
+// var cost = 120;
+
+
+// var cost = 120 , profit = 77;
+
+
+
+
+
+// var _grpupName;
+// var $bicycle;
+// var ABN;
+// var parti_x_y;
+// var partix_y;
+// var abc$1234;
+// var easy;
+
+// var, let , const 
+
+
+
+count = 4;
+var result = false ;
+
+if (condion === true ){
+    result = true 
+}
+
+console.log(result);
