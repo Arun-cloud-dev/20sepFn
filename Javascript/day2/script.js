860587deb5c79d08d1b7148f8447a6a0c02149f4
Arun raj <riderarunraj@live.com>
2024-10-18 17:39:26 +0530

    day 2
    js

---
+++ Javascript/day2/script.js
@@ -0,0 +1,104 @@
+// global variables 
+
+// var value = 50; 
+
+// function a(){
+//     alert(value);
+// }
+// function b ()
+// {
+//     alert(value)
+// ;}
+
+
+// function m (){
+//     window.value=100; 
+// }
+// function n (){
+//     alert(window.value)
+// }
+
+
+
+// Non - primitive data types 
+// Object
+
+// let car = {
+//     brand :"benz",
+//     model:"A1",
+//     color:"Red",
+//     start: function (){
+//         console.log("car is starting");
+//     },
+//     stop : function(){
+//         console.log("car is stopping ");
+//     },
+// }
+
+// // access property 
+
+// console.log(car.brand);
+
+// //calling methords 
+
+// car.start();
+// car.stop();
+
+// let student = {
+//     name: "arun",
+//     age: 23,
+//     greet: function(){
+//         console.log("hello, i am "+ this.name);
+//     }
+// }
+
+// student.greet()
+// console.log();
+
+// constructor function in object
+
+// function car (brand, model , color){
+//     this.brand = brand;
+//     this.color = model;
+//     model = model;
+//     start = function(){
+//         console.log("there is no key");     
+//     }
+// }
+// let car1 =new car ("Honda", "civic ", "red") 
+
+// let car2 =new car("ford", "figo", "white")
+
+// console.log(car1,car2);
+
+
+
+// Array
+var empty = [];
+
+
+var days = ["sunday", "moonday"]
+
+
+var mixed = [false , 2.70, "string"]
+
+
+var suffix = "tree";
+var trees = ["oak"+suffix, "Neem"+suffix];
+
+
+// 2 dimentional array 
+
+const  arrray2 = [[1,{x:1,y:2}], [2,{x:3, y:4}]];
+
+
+var colors = ["red", "yellow", undefined];
+
+
+var hobbies = [,"art"]
+
+
+console.log (arrray2[1][1]);
+
+
+// RegExp
