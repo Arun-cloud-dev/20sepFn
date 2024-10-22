d4e5891b12d337479ac2721b2a0569ac7650dbe4
Arun raj <riderarunraj@live.com>
2024-10-22 15:42:04 +0530

    day 1 - 3

---
+++ Javascript/Day3/script.js
@@ -0,0 +1,153 @@
+// operators
+
+
+
+
+//  + , _ , * , / , % , ++ , -- , 
+
+
+
+// let   a =  10;
+// let b = a ++;
+// console.log(b);
+
+
+
+// let b  =  12;
+// let a = 45; 
+
+
+
+// var , let, const 
+
+// console.log(a += b) 
+
+
+// a = a+b   
+
+
+// variables
+
+
+// blocked scope 
+
+// let a = 20 
+// {
+//    let  a = 10 ;
+//    console.log(a);
+// }
+
+
+// let  declared before use 
+
+
+// let b ; 
+// b = 550;
+
+
+// var n ;
+// n = 64569;
+
+// var a = 65;
+// {
+//   var  a = 564;
+// }
+
+// console.log(a);
+
+
+// let c = 55;
+// {
+//    let  c = 566;
+// }
+// console.log(c);
+
+
+// let and const cannot be redeclared ; 
+
+// let  a = 45;
+// console.log(a);
+// {
+// let a = 65; 
+// console.log(a);
+// }
+
+
+// const a = 456.356;
+// const a = 526;
+
+// const b  = 456.677;
+
+// const c  = a + b;
+
+// console.log(c);
+
+
+// must use 
+// array 
+// objects 
+// new functiom 
+// regExp 
+
+// const array 
+// var  book = ['ars', 'srs ', 'bmw'];
+
+// book = 'asd', 
+// console.log(book);
+
+// book [0] =   'Benz';
+
+
+
+// book.push("ar")
+
+// console.log(book);
+
+
+
+
+
+// var ar = "arun"
+
+// var ar; 
+
+
+// console.log(ar);
+
+
+
+// Syntax - function 
+// function name (parm1, parm2, par3){
+//     //code will be exicuted 
+// }
+
+
+let car1 =new car ("Honda", "civic ", "red") 
+
+let car2 =new car("ford", "figo", "white")
+
+
+function car (brand, model , color){
+    this.brand = brand;
+    this.color = model;
+    model = model;
+    start = function(){
+        console.log("there is no key");     
+    }
+}
+
+
+console.log(car1,car2);
+
+
+
+
+
+// const x = arun(3 , 6)
+
+
+// function arun (a, b ){
+//     return a*b ; 
+// }
+
+// console.log(x);
