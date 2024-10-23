4d153c9cde756e33b22ebc7b456e1e020fe86b1f
Arun raj <riderarunraj@live.com>
2024-10-23 18:48:28 +0530

    day 4 js

---
+++ Javascript/Day4/Script.js
@@ -0,0 +1,126 @@
+// let age  = 7;
+
+
+// console.log("arun  "+age+ "years old ");
+
+
+// ///string interpolation 
+// console.log( ` tom is ${age} years old `);
+
+
+
+
+// coditions 
+
+// const isMailSent = true;
+
+// if (isMailSent){
+//     console.log('mail is sent to ar');
+// }
+// syntax 
+
+
+
+
+
+// Use if to specify a block of code to be executed, if a specified condition is true
+// syntax 
+
+// if (condition) {
+//     //  block of code to be executed if the condition is true
+//   }
+
+// let age = 20;
+// if (age >= 18){
+//     console.log("you are an adult  ");
+// }
+
+
+// Use else to specify a block of code to be executed, if the same condition is false
+
+// let age = 17;
+
+
+// if (age >= 18){
+//     console.log("you are an adult ");
+// }else {
+//     console.log("you are not an adult ");
+// }
+
+
+
+
+
+
+
+// // Use else if to specify a new condition to test, if the first condition is false
+// syntax
+
+
+// if (condition1) {
+//     //  block of code to be executed if condition1 is true
+//   } else if (condition2) {
+//     //  block of code to be executed if the condition1 is false and condition2 is true
+//   } else {
+//     //  block of code to be executed if the condition1 is false and condition2 is false
+//   }
+
+// let  age = 10;
+
+
+// if (age >= 18){
+//     console.log('You are an adult ');
+// }else if (age >= 13){
+//     console.log("you are a teen");
+// } else {
+//     console.log("you are a child " );
+// }
+
+
+// Use switch to specify many alternative blocks of code to be executed
+
+//syntx 
+
+
+// switch(expression) {
+//     case x:
+//       // code block
+//       break;
+//     case y:
+//       // code block
+//       break;
+//     default:
+//       // code block
+//   }
+
+let day = 5;
+
+
+switch (day ){
+    case 1:
+    console.log("Monday");
+    break;
+
+        case 2:
+        console.log("Tuesday");
+        break;
+
+        case 3:
+        console.log("Saturday");
+        break;
+        
+        case 4:
+        console.log("weekemnd");
+                break;
+                case 5:
+                    console.log("weekend");
+                    break;
+
+                    default:
+                        console.log("invalid day ");
+                           
+
+} 
+
+
+
---
+++ Javascript/Day4/loop.js
@@ -0,0 +1,68 @@
+// loop:
+
+
+// for loop 
+
+// syntax
+// for (inilization ; conditon ; incriment/decrement  ){
+//     //code to be exicuted 
+// }
+
+//  for  (let i = 0;  i < 5 ; i++){
+
+//     console.log(i);
+//  }
+
+
+
+//while loop 
+
+
+
+// while (condition){
+//     //code to be exicuted 
+// }
+
+// let i = 0;
+
+// while (i < 5 ){
+//     console.log(i);
+//     i++
+// }
+
+
+
+
+// do while 
+
+
+// do {
+//     //code exicuted 
+// } while (condition )
+
+
+
+
+
+
+
+
+// let i = 2
+// do {
+//     console.log(i);
+//     i++;
+// }while (i < 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
