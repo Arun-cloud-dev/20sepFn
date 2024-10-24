dfa4a4b81007cbbad66ca7bc96b9ef4b3da6d027
Arun raj <riderarunraj@live.com>
2024-10-24 18:39:07 +0530

    js day 5

---
+++ Javascript/Day5/script.js
@@ -0,0 +1,99 @@
+//object litral notation 
+//stntax
+// const objetsName = {
+//     key1: value1,
+//     key2 : value2,
+// }
+
+
+// const person = {
+//     name : "john", 
+//     age :30,
+//     city: "chennai",
+//     greet : function ()
+// {
+// return 'message ' + this.name;
+// }}
+
+
+// // dot notation
+
+// console.log(person.age);
+// // bracket notation
+// console.log(person['name']);
+
+
+//add 
+
+// person.country = "india"
+// delete person.greet;
+// console.log(person);
+
+
+
+
+
+// console.log(person.name);
+// console.log(person.age);
+
+// console.log(person.ar);
+
+
+
+
+
+
+
+
+
+
+// new Object() constructors
+
+//syntax
+
+// const objetsName = new Object();
+// objetsName.key1 = value1;
+
+
+
+// const car = new Object() ;
+
+
+// car.brand = "tata";
+// car.model = 'nano';
+
+// car.year = 2011 ;
+
+
+// console.log(car.brand);
+// console.log(car.year);
+
+
+
+
+
+
+// const person = {
+//     name : "john", 
+//     age :30,
+//     city: "chennai",
+//     address: {
+//         street :" st tank st ",
+//         city:"xxxxxxx",
+//         xip:54946,
+//     }   
+// }
+
+// // console.log(person.address.city);
+
+
+// for (let key in person) {
+//     console.log(key + ": " + person[key]);
+//   }
+
+
+
+const d = new Date();
+
+console.log(d.getFullYear()) ;
+console.log(d.getHours());
