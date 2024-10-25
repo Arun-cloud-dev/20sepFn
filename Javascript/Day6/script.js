2f46d3e41072a544bfcfe77ce4cd932e31e5b02d
Arun raj <riderarunraj@live.com>
2024-10-25 18:25:14 +0530

    Day 6

---
+++ Javascript/Day6/script.js
@@ -0,0 +1,42 @@
+// const para = document.getElementsByTagName('p');
+
+
+
+// const newelement = document.createElement('p');
+
+// newelement.textContent = "hey new text ";
+
+// document.getElementById('ar').appendChild(newelement)
+
+
+
+
+const newpara = document.createElement("p");
+
+
+newpara.textContent = "this is the text area"
+
+document.getElementById("ar").appendChild(newpara)
+
+
+// for (let i=0; i < para.length; i++ ){
+//     para[i].style.color = "green"
+// }
+
+
+// document.getElementById("heading").style.color = "blue"
+
+
+// const ar = document.getElementsByClassName('ar')
+
+
+
+
+// for (let i=0; i < ar.length; i++ ){
+// ar[i].style.color = "yellowgreen"
+// }
+
+
+
+
+// document.removeChild(div);
